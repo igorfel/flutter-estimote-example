51d03b808ccad7c680022365dfd0124b7cf8bab3
igorfel <appsigor@gmail.com>
2018-07-20 09:21:24 -0300

    Implementation of flutterBlue

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
+import 'package:flutter_estimote/widgets.dart';
+
+import 'dart:async';
 
 void main() => runApp(new MyApp());
 
@@ -44,74 +47,309 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  
-  FlutterBlue flutterBlue = FlutterBlue.instance;
+  FlutterBlue _flutterBlue = FlutterBlue.instance;
 
-  // Scanning 
-  var scanSubscription;
+  /// Scanning
+  StreamSubscription _scanSubscription;
   Map<DeviceIdentifier, ScanResult> scanResults = new Map();
   bool isScanning = false;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  /// State
+  StreamSubscription _stateSubscription;
+  BluetoothState state = BluetoothState.unknown;
+
+  /// Device
+  BluetoothDevice device;
+  bool get isConnected => (device != null);
+  StreamSubscription deviceConnection;
+  StreamSubscription deviceStateSubscription;
+  List<BluetoothService> services = new List();
+  Map<Guid, StreamSubscription> valueChangedSubscriptions = {};
+  BluetoothDeviceState deviceState = BluetoothDeviceState.disconnected;
+
+  @override
+  void initState() {
+    super.initState();
+    // Immediately get the state of FlutterBlue
+    _flutterBlue.state.then((s) {
+      setState(() {
+        state = s;
+      });
+    });
+    // Subscribe to state changes
+    _stateSubscription = _flutterBlue.onStateChanged().listen((s) {
+      setState(() {
+        state = s;
+      });
     });
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
+  void dispose() {
+    _stateSubscription?.cancel();
+    _stateSubscription = null;
+    _scanSubscription?.cancel();
+    _scanSubscription = null;
+    deviceConnection?.cancel();
+    deviceConnection = null;
+    super.dispose();
+  }
+
+  _startScan() {
+    _scanSubscription = _flutterBlue
+        .scan(
+      timeout: const Duration(seconds: 5),
+      /*withServices: [
+          new Guid('0000180F-0000-1000-8000-00805F9B34FB')
+        ]*/
+    )
+        .listen((scanResult) {
+      print('localName: ${scanResult.advertisementData.localName}');
+      print(
+          'manufacturerData: ${scanResult.advertisementData.manufacturerData}');
+      print('serviceData: ${scanResult.advertisementData.serviceData}');
+      setState(() {
+        scanResults[scanResult.device.id] = scanResult;
+      });
+    }, onDone: _stopScan);
+
+    setState(() {
+      isScanning = true;
+    });
+  }
+
+  _stopScan() {
+    _scanSubscription?.cancel();
+    _scanSubscription = null;
+    setState(() {
+      isScanning = false;
+    });
+  }
+
+  _connect(BluetoothDevice d) async {
+    device = d;
+    // Connect to device
+    deviceConnection = _flutterBlue
+        .connect(device, timeout: const Duration(seconds: 4))
+        .listen(
+          null,
+          onDone: _disconnect,
+        );
+
+    // Update the connection state immediately
+    device.state.then((s) {
+      setState(() {
+        deviceState = s;
+      });
+    });
+
+    // Subscribe to connection changes
+    deviceStateSubscription = device.onStateChanged().listen((s) {
+      setState(() {
+        deviceState = s;
+      });
+      if (s == BluetoothDeviceState.connected) {
+        device.discoverServices().then((s) {
+          setState(() {
+            services = s;
+          });
+        });
+      }
+    });
+  }
+
+  _disconnect() {
+    // Remove all value changed listeners
+    valueChangedSubscriptions.forEach((uuid, sub) => sub.cancel());
+    valueChangedSubscriptions.clear();
+    deviceStateSubscription?.cancel();
+    deviceStateSubscription = null;
+    deviceConnection?.cancel();
+    deviceConnection = null;
+    setState(() {
+      device = null;
+    });
+  }
+
+  _readCharacteristic(BluetoothCharacteristic c) async {
+    await device.readCharacteristic(c);
+    setState(() {});
+  }
+
+  _writeCharacteristic(BluetoothCharacteristic c) async {
+    await device.writeCharacteristic(c, [0x12, 0x34],
+        type: CharacteristicWriteType.withResponse);
+    setState(() {});
+  }
+
+  _readDescriptor(BluetoothDescriptor d) async {
+    await device.readDescriptor(d);
+    setState(() {});
+  }
+
+  _writeDescriptor(BluetoothDescriptor d) async {
+    await device.writeDescriptor(d, [0x12, 0x34]);
+    setState(() {});
+  }
+
+  _setNotification(BluetoothCharacteristic c) async {
+    if (c.isNotifying) {
+      await device.setNotifyValue(c, false);
+      // Cancel subscription
+      valueChangedSubscriptions[c.uuid]?.cancel();
+      valueChangedSubscriptions.remove(c.uuid);
+    } else {
+      await device.setNotifyValue(c, true);
+      // ignore: cancel_subscriptions
+      final sub = device.onValueChanged(c).listen((d) {
+        setState(() {
+          print('onValueChanged $d');
+        });
+      });
+      // Add to map
+      valueChangedSubscriptions[c.uuid] = sub;
+    }
+    setState(() {});
+  }
+
+  _refreshDeviceState(BluetoothDevice d) async {
+    var state = await d.state;
+    setState(() {
+      deviceState = state;
+      print('State refreshed: $deviceState');
+    });
+  }
+
+  _buildScanningButton() {
+    if (isConnected || state != BluetoothState.on) {
+      return null;
+    }
+    if (isScanning) {
+      return new FloatingActionButton(
+        child: new Icon(Icons.stop),
+        onPressed: _stopScan,
+        backgroundColor: Colors.red,
+      );
+    } else {
+      return new FloatingActionButton(
+          child: new Icon(Icons.search), onPressed: _startScan);
+    }
+  }
+
+  _buildScanResultTiles() {
+    return scanResults.values
+        .map((s) => new ListTile(
+              title: new Text(s.device.name),
+              subtitle: new Text(s.device.id.toString()),
+              leading: new Text(s.rssi.toString()),
+              onTap: () => _connect(s.device),
+            ))
+        .toList();
+  }
+
+  List<Widget> _buildServiceTiles() {
+    return services
+        .map(
+          (s) => new ServiceTile(
+                service: s,
+                characteristicTiles: s.characteristics
+                    .map(
+                      (c) => new CharacteristicTile(
+                            characteristic: c,
+                            onReadPressed: () => _readCharacteristic(c),
+                            onWritePressed: () => _writeCharacteristic(c),
+                            onNotificationPressed: () => _setNotification(c),
+                            descriptorTiles: c.descriptors
+                                .map(
+                                  (d) => new DescriptorTile(
+                                        descriptor: d,
+                                        onReadPressed: () => _readDescriptor(d),
+                                        onWritePressed: () =>
+                                            _writeDescriptor(d),
+                                      ),
+                                )
+                                .toList(),
+                          ),
+                    )
+                    .toList(),
+              ),
+        )
+        .toList();
+  }
+
+  _buildActionButtons() {
+    if (isConnected) {
+      return <Widget>[
+        new IconButton(
+          icon: const Icon(Icons.cancel),
+          onPressed: () => _disconnect(),
+        )
+      ];
+    }
+  }
+
+  _buildAlertTile() {
+    return new Container(
+      color: Colors.redAccent,
+      child: new ListTile(
+        title: new Text(
+          'Bluetooth adapter is ${state.toString().substring(15)}',
+          style: Theme.of(context).primaryTextTheme.subhead,
+        ),
+        trailing: new Icon(
+          Icons.error,
+          color: Theme.of(context).primaryTextTheme.subhead.color,
+        ),
       ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+    );
+  }
+
+  _buildDeviceStateTile() {
+    return new ListTile(
+        leading: (deviceState == BluetoothDeviceState.connected)
+            ? const Icon(Icons.bluetooth_connected)
+            : const Icon(Icons.bluetooth_disabled),
+        title: new Text('Device is ${deviceState.toString().split('.')[1]}.'),
+        subtitle: new Text('${device.id}'),
+        trailing: new IconButton(
+          icon: const Icon(Icons.refresh),
+          onPressed: () => _refreshDeviceState(device),
+          color: Theme.of(context).iconTheme.color.withOpacity(0.5),
+        ));
+  }
+
+  _buildProgressBarTile() {
+    return new LinearProgressIndicator();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var tiles = new List<Widget>();
+    if (state != BluetoothState.on) {
+      tiles.add(_buildAlertTile());
+    }
+    if (isConnected) {
+      tiles.add(_buildDeviceStateTile());
+      tiles.addAll(_buildServiceTiles());
+    } else {
+      tiles.addAll(_buildScanResultTiles());
+    }
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: const Text('FlutterBlue'),
+          actions: _buildActionButtons(),
+        ),
+        floatingActionButton: _buildScanningButton(),
+        body: new Stack(
           children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            (isScanning) ? _buildProgressBarTile() : new Container(),
+            new ListView(
+              children: tiles,
+            )
           ],
         ),
       ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
